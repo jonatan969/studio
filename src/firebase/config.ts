15ccd0fab4822a597527980ea9a410740dac1129
LaRataMode <jonatanaries88@gmail.com>
2025-12-30 04:37:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5200996718-8d44e",
+  "appId": "1:979661946686:web:cbb6d708058cccc149b66f",
+  "apiKey": "AIzaSyA7gYSy9ujxKdtwB0h1IuGAH5_xctp-b1U",
+  "authDomain": "studio-5200996718-8d44e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "979661946686"
+};
