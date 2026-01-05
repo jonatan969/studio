898ca978fc0a30b2be16d3d60ce2a202e35e76c8
LaRataMode <jonatanaries88@gmail.com>
2026-01-05 01:58:10 +0000

    Probemos con otra fuente, una mas normal pero esteticamente limpia

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Rajdhani', 'sans-serif'],
-        headline: ['Orbitron', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
